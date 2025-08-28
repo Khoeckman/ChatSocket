e11606612896b9f85d5638c54e8c6e5e0eb45be8
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2025-08-29 00:48:10 +0200

    ChatSocketController WIP

--- ChatSocketController/js/ChatSocketController.js
+++ ChatSocketController/js/ChatSocketController.js
@@ -6,10 +6,10 @@ export default class ChatSocketController extends WebSocket {
       this.onerror(err)
     }
 
-    // Todo: validate
-    console.log(this.url)
+    if (typeof secretKey !== 'string' && !secretKey.length) throw new TypeError('secretKey is not a string')
     this.secretKey = secretKey.replaceAll(' ', '')
-    this.log = logElement
+
+    this.log = secretKey instanceof HTMLElement ? logElement : {}
   }
 
   // Prepare to send to ChatSocket
@@ -24,7 +24,7 @@ export default class ChatSocketController extends WebSocket {
   }
 
   onopen(ev) {
-    log.innerHTML += `<p style="color: lime">+ Connected to ${ev.target.url}</p`
+    log(`<p style="color: lime">+ Connected to ${ev.target.url}</p`)
     this.send(this.secretKey + ' AUTH')
   }
 
@@ -32,7 +32,7 @@ export default class ChatSocketController extends WebSocket {
     let { secretKey, type, value } = ChatSocketController.decodeMessage(ev.data)
     const isAuth = this.secretKey === secretKey
 
-    log.innerHTML += `<p><b style="color: lime">-&#x3E;</b> ${ev.data}</p>`
+    log(`<p><b style="color: lime">-&#x3E;</b> ${ev.data}</p>`)
 
     if (!isAuth) {
       if (type === 'AUTH') {
@@ -41,8 +41,6 @@ export default class ChatSocketController extends WebSocket {
       }
       return
     }
-
-    if (typeof ws.onReceive === 'function') ws.onReceive(type, value, settings)
   }
 
   send(type, value) {
@@ -51,16 +49,24 @@ export default class ChatSocketController extends WebSocket {
     const secretKey = this.isAuth ? this.secretKey : '*'
     const message = ChatSocketController.encodeMessage(secretKey, type, value)
     super.send(message)
-    log.innerHTML += `<p><b style="color: red">&#x3C;-</b> ${value}</p>`
+    log(`<p><b style="color: red">&#x3C;-</b> ${value}</p>`)
   }
 
   onerror(err) {
-    console.error('WebSocket error:', err)
-    log.innerHTML += `<p style="color: red">WebSocket Error: ${err}</p>`
+    console.error('WebSocket Error:', err)
+    log(`<p style="color: red">WebSocket Error: ${err}</p>`)
   }
 
   onclose() {
-    log.innerHTML += `<p style="color: red">+ Disconnected from ${ev.target.url}</p`
+    log(`<p style="color: red">+ Disconnected from ${ev.target.url}</p`)
+  }
+
+  log(msg) {
+    if (!log) {
+      console.log(msg)
+      return
+    }
+    log(`<p><b style="color: red">&#x3C;-</b> ${value}</p>`)
   }
 
   logClear() {
--- ChatSocketController/js/main.js
+++ ChatSocketController/js/main.js
@@ -1,9 +1,18 @@
 import ChatSocketController from './ChatSocketController.js'
 
+const WEBSOCKET_URL = 'wss://chatsocket-a1xp.onrender.com/'
+const WEBSOCKET_SECRET_KEY = '2721c210b42f652f56f6f95c156f2b5b'
+
 let ws = null
+const secretKeyInput = document.getElementById('secretKey')
+const log = document.getElementById('log')
+
+secretKeyInput.addEventListener('input', function () {
+  this.value
+})
 
 // Autoconnect
 setInterval(() => {
   if (ws && (ws.readyState === WebSocket.CLOSING || ws.readyState === WebSocket.CLOSED)) return
-  ws = new ChatSocketController('ws://localhost:47576', '2721c210b42f652f56f6f95c156f2b5b')
+  ws = new ChatSocketController(WEBSOCKET_URL, SECRET_KEY, log)
 }, 2000)
