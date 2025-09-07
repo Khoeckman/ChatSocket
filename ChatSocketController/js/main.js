27b32d2309221075e3b0ea41ddcee5504887b5b0
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2025-09-07 18:35:37 +0200

    Rename secretKey to secret

--- ChatSocketController/js/main.js
+++ ChatSocketController/js/main.js
@@ -1,10 +1,10 @@
 import ChatSocketController from './ChatSocketController.js'
 
-const WEBSOCKET_SERVER_URL = 'ws://localhost:47576' //'wss://chatsocket-a1xp.onrender.com/'
-const WEBSOCKET_SECRET_KEY = '15f5ccd5987f59418bbc534a02a6e36'
+const WEBSOCKET_URL = 'ws://localhost:47576' //'wss://chatsocket-a1xp.onrender.com/'
+const WEBSOCKET_SECRET = '15f5ccd5987f59418bbc534a02a6e36'
 
 let ws = null
-const secretKeyInput = document.getElementById('secretKey')
+const secretKeyInput = document.getElementById('secret')
 const log = document.getElementById('log')
 
 secretKeyInput.addEventListener('input', function () {
@@ -14,5 +14,5 @@ secretKeyInput.addEventListener('input', function () {
 // Autoconnect
 setInterval(() => {
   if (ws && (ws.readyState === WebSocket.CLOSING || ws.readyState === WebSocket.CLOSED)) return
-  ws = new ChatSocketController(WEBSOCKET_URL, SECRET_KEY, log)
+  ws = new ChatSocketController(WEBSOCKET_URL, WEBSOCKET_SECRET, log)
 }, 2000)
