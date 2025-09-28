6c31a56299ee74dd3966f30cc92c19dd76fc1411
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2025-09-28 03:39:54 +0200

    Add form functionality to send messages to WSS

--- ChatSocketWebClient/js/index.js
+++ ChatSocketWebClient/js/index.js
@@ -1,5 +1,6 @@
 import ChatSocketWebClient from './ChatSocketWebClient.js'
 
+// WebSocket
 let ws = null
 
 // Autoconnect
@@ -12,3 +13,19 @@ setInterval(() => {
     document.getElementById('log')
   )
 }, 2000)
+
+// Outgoing
+const ChatSocketForm = document.getElementById('ChatSocket')
+
+ChatSocketForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  if (!ws || ws.readyState !== ChatSocketWebClient.OPEN) return
+
+  const form = e.target
+  const type = form.elements['type'].value
+  const message = form.elements['message'].value
+  const data = form.elements['data'].value
+
+  ws.sendEncoded(type, message, data)
+})
