b82b4ebee961f2cef96ef96690aafa8803116649
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2025-10-07 22:06:29 +0200

    Support custom event listeners on top of `onmessageFn`

--- ChatSocketWebClient/js/ChatSocketWebClient.js
+++ ChatSocketWebClient/js/ChatSocketWebClient.js
@@ -89,6 +89,8 @@ class ChatSocketWebClient extends WebSocket {
     }
 
     if (typeof this.onmessageFn === 'function') this.onmessageFn.call(this, type, message, data)
+
+    this.dispatchEvent(new CustomEvent('message', { detail: { type, message, data } }))
   }
 
   sendEncoded(type, message, data = {}) {
