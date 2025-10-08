9edf39e0c71db59b0db96d20a6be6809d4c48eff
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2025-10-08 03:00:09 +0200

    Write a demo of how to use the App class

--- ChatSocketWebClient/js/app.js
+++ ChatSocketWebClient/js/app.js
@@ -1,65 +1,112 @@
-let connectTimeout
+class MinecraftApp {
+  inServer = false
+  inWorld = false
+  connectTimeout = 0
 
-/**
- * Handles incoming WebSocket messages from {@link ChatSocketWebClient}.
- *
- * When registered with `ws.addEventListener("message", onmessage)`,
- * this function is invoked with `this` bound to the active WebSocket
- * client instance.
- *
- * @this {ChatSocketWebClient} The WebSocket client that received the message.
- * @param {string} type - The high-level message type (e.g. "AUTH", "CHAT", "EXEC").
- * @param {string} message - The human-readable message string from the server.
- * @param {Object<string, any>} [data={}] - Structured message data payload.
- *
- * @example
- * ws.addEventListener("message", onmessage)
- *
- * function onmessage(type, message, data) {
- *   this.log(this.url, type, message, data)
- * }
- */
-function onmessage(type, message, data = {}) {
-  const rawMessage = Utils.removeMcFormatting(message)
+  constructor(ws) {
+    if (!(ws instanceof ChatSocketWebClient)) throw new TypeError('ws is not an instance of ChatSocketWebClient')
+    this.ws = ws
 
-  switch (type) {
-    case 'DEBUG':
-    case 'AUTH':
-    case 'CHANNEL':
-    case 'CHANNELS':
-    case 'CLIENTS':
-    case 'SERVER':
-    case 'CONNECT':
-      connection = true
-      break
-    case 'DISCONNECT':
-      clearTimeout(connectTimeout)
-      // Attempt reconnect after 2s
-      connectTimeout = setTimeout(() => ws.sendEncoded('CONNECT', 'play.hypixel.net'), 2000)
-      break
-    case 'WORLD':
-    case 'LOAD':
-    case 'UNLOAD':
-    case 'CLIENT_SAY':
-      onchat(rawMessage)
-      break
-    case 'SERVER_SAY':
-    case 'CLIENT_CMD':
-    case 'SERVER_CMD':
-    case 'EXEC':
-      break
-    default:
-      ws.log(`&cWebSocketError: &fUnsupported message type '${type}'`)
-      break
+    this.ws.ondecoded = this.onmessage.bind(this)
+
+    this.ws.addEventListener('open', () => {
+      this.ws.sendEncoded('SERVER')
+      this.ws.sendEncoded('WORLD')
+    })
+
+    setInterval(() => {
+      ws.sendEncoded('SERVER_CMD', 'tp ~ ~ ~')
+    }, 1002)
   }
-}
 
-function onchat(rawMessage) {
-  let regex = /\[CS\]\s+tp\s+(\s\S)+/
+  /**
+   * Handles incoming WebSocket messages from {@link ChatSocketWebClient}.
+   *
+   * When registered with `ws.ondecoded = onmessage`,
+   * this function is invoked with `this` bound to the active WebSocket
+   * client instance.
+   *
+   * @this {ChatSocketWebClient} The WebSocket client that received the message.
+   * @param {string} type - The high-level message type (e.g. "AUTH", "CHAT", "EXEC").
+   * @param {string} message - The human-readable message string from the server.
+   * @param {Object<string, any>} [data] - Structured message data payload.
+   *
+   * @example
+   * ws.addEventListener("message", onmessage)
+   *
+   * function onmessage(type, message, data) {
+   *   this.log(this.url, type, message, data)
+   * }
+   */
+  onmessage(type, message, data) {
+    const rawMessage = Utils.removeMcFormatting(message)
 
-  if (regex.test(rawMessage)) {
-    const [_, args] = regex.exec(rawMessage)
-    ws.sendEncoded('CMD', `tp ${args}`)
-    return
+    switch (type) {
+      case 'DEBUG':
+        break
+      case 'AUTH':
+        break
+      case 'CHANNEL':
+        if (data.action === 'leave') {
+          this.inServer = false
+          this.inWorld = false
+          break
+        }
+
+        if (data.action === 'join') {
+          this.ws.sendEncoded('SERVER')
+          this.ws.sendEncoded('WORLD')
+        }
+        break
+      case 'CHANNELS':
+        break
+      case 'CLIENTS':
+        break
+      case 'SERVER':
+        this.inServer = !!data.server?.ip?.length
+        break
+      case 'CONNECT':
+        this.inServer = !!data.server?.ip?.length
+        break
+      case 'DISCONNECT':
+        this.inServer = false
+
+        clearTimeout(this.connectTimeout)
+        // Attempt reconnect after 2s
+        this.connectTimeout = setTimeout(() => (!inServer ? ws.sendEncoded('CONNECT', 'play.hypixel.net') : null), 2000)
+        break
+      case 'WORLD':
+        this.inWorld = message !== 'null'
+      case 'LOAD':
+        break
+      case 'UNLOAD':
+        break
+      case 'CLIENT_SAY':
+        this.teleporter(rawMessage)
+        break
+      case 'SERVER_SAY':
+        break
+      case 'CLIENT_CMD':
+        break
+      case 'SERVER_CMD':
+        break
+      case 'EXEC':
+        break
+      default:
+        ws.log(`&cWebSocketError: &fUnsupported message type '${type}'`)
+        break
+    }
+  }
+
+  // -> SERVER_SAY "... [CS] tp {args}"
+  // <- SERVER_CMD "tp {args}"
+  teleporter(rawMessage) {
+    const regex = /\[CS\]\s+\/*tp\s+([\s\S]+)/
+
+    if (regex.test(rawMessage)) {
+      const [_, args] = regex.exec(rawMessage)
+      this.ws.sendEncoded('SERVER_CMD', `tp ${args.trim()}`)
+      return
+    }
   }
 }
